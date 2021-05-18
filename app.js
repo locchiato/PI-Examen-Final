7338a7ff8ebb2bca3fb712f05da00dc2717f89cd
Leandro Occhiato <leandro.occhiato@gmail.com>
2021-05-18 02:03:30 -0300

    [MOD] Se aplican buenas practicas.

--- app.js
+++ app.js
@@ -8,18 +8,17 @@ let carrera = {
     bicicletas: bicis,
     bicisPorTanda: 7,
     bicicletasHabilitadas: function() {
-        return this.bicicletas.filter(e => !e.doppingPositivo);
+        return this.bicicletas.filter(bici => !bici.doppingPositivo);
     },
-    listarBicicletas: function(lista) {
-        console.log("\n----- Lista de Bicicletas -----");
-        lista.forEach((cv, i) => {
-            console.log("Id: " + cv.id + ", " +
-                "rodado: " + cv.rodado + ", " +
-                "peso en Kg: " + cv.pesoEnKg + ", " +
-                "estado: " + (cv.doppingPositivo ? "descalificado" : "habilitado"))
-            console.log("----- ------------------- -----");
+    listarBicicletas: function(bicis) {
+        console.log("----- Lista de Bicicletas -----");
+        bicis.forEach(bici => {
+            console.log("Id: " + bici.id + ", " +
+                "rodado: " + bici.rodado + ", " +
+                "peso en Kg: " + bici.pesoEnKg + ", " +
+                "estado: " + (bici.doppingPositivo ? "descalificado" : "habilitado"))
         })
-        console.log("----- ------------------- -----");
+        console.log("----- ------------------- -----\n");
     },
     buscarPorId: function(id) {
         return this.bicicletas.find(bici => bici.id === id)
@@ -28,22 +27,18 @@ let carrera = {
         return this.bicicletasHabilitadas().filter(bici => bici.rodado <= rodado)
     },
     ordenarPorRodado: function() {
-        return this.bicicletas.sort((a, b) => a.rodado - b.rodado)
+        return this.bicicletas.sort((bici1, bici2) => bici1.rodado - bici2.rodado)
     },
     generarTanda: function(rodado, pesoEnKg) {
-        return this.bicicletasHabilitadas().filter(bici =>
-            bici.rodado <= rodado &&
+        return this.buscarPorRodado(rodado).filter(bici =>
             bici.pesoEnKg <= pesoEnKg).slice(0, this.bicisPorTanda)
     },
-    calcularPodio: function(tanda) {
-        tanda = tanda ? tanda : this.generarTanda(100, 10);
-
-        tanda.sort((a, b) => b.puntaje - a.puntaje)
-            .slice(0, 3)
-            .forEach((el, i) => {
-                console.log("El " + (i == 0 ? "ganador " : i == 1 ? "segundo puesto " : "tercer puesto") +
-                    "es" + (i > 0 ? " para: " : ": ") + el.ciclista + ", con un puntaje de: " + el.puntaje);
-            })
+    calcularPodio: function() {
+        const podio = this.generarTanda(100, 10).sort((bici1, bici2) => bici2.puntaje - bici1.puntaje)
+            .slice(0, 3);
+        console.log("El ganador es: " + podio[0].ciclista + ", con un puntaje de: " + podio[0].puntaje +
+                    "\nEl segundo puesto es para: " + podio[1].ciclista + ", con un puntaje de: " + podio[1].puntaje +
+                    "\nEl tercer puesto es para: " + podio[2].ciclista + ", con un puntaje de: " + podio[2].puntaje);
     }
 }
 
@@ -56,4 +51,4 @@ carrera.listarBicicletas(carrera.buscarPorRodado(65));
 carrera.listarBicicletas(carrera.ordenarPorRodado());
 carrera.listarBicicletas(carrera.generarTanda(100, 10));
 
-carrera.calcularPodio(carrera.generarTanda(100, 10));
+carrera.calcularPodio();
